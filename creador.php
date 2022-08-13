dfebbcb234b65798cf22ce8ba3fc2b709568a2a7
Eli <perwin119@gmail.com>
2022-08-13 17:32:00 -0500

    .

--- creador.php
+++ creador.php
@@ -1,3 +1,19 @@
 <?php
 
-echo "soy el creador";
+//importamos la clase de inicio de la app
+require_once('app.class.php');
+//importamos la clase de template
+require_once('template.class.php');
+
+//creamos un objeto de tipo template para asi poder crear una plantilla por cada vista
+$child = new Template("views/creador.html");
+
+//incluyo la vista de inicio para crear la ruta de carpetas falsas
+$view = new Template("views/template.html", [
+    "nombre" => "Erwin Eli",
+    "child" => $child
+]);
+
+echo $view;
+
+//echo "soy el creador";
--- reglas.php
+++ reglas.php
@@ -1,3 +1,17 @@
 <?php
 
-echo "soy la coleccion de libros de reglas";
+//importamos la clase de inicio de la app
+require_once('app.class.php');
+//importamos la clase de template
+require_once('template.class.php');
+
+//creamos un objeto de tipo template para asi poder crear una plantilla por cada vista
+$child = new Template("views/reglas.html");
+
+//incluyo la vista de inicio para crear la ruta de carpetas falsas
+$view = new Template("views/template.html", [
+    "nombre" => "Erwin Eli",
+    "child" => $child
+]);
+
+echo $view;
